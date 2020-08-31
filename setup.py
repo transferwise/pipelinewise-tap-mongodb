3e84378ba1a596acc912ccbcc006a7013841ed3c
Samira <54845154+Samira-El@users.noreply.github.com>
2020-08-31 16:02:42 +0300

    bump minor

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
     long_desc = fh.read()
 
 setup(name='pipelinewise-tap-mongodb',
-      version='1.1.0',
+      version='1.2.0',
       description='Singer.io tap for extracting data from MongoDB - Pipelinewise compatible',
       long_description=long_desc,
       long_description_content_type='text/markdown',
